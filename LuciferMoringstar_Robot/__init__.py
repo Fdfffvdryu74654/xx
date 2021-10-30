61de167415791488249b2f9f84e920d0fac01ac9
Xrsirx5 <91307602+Xrsirx5@users.noreply.github.com>
2021-10-30 16:18:25 +0530

    Update __init__.py

--- LuciferMoringstar_Robot/__init__.py
+++ LuciferMoringstar_Robot/__init__.py
@@ -15,15 +15,7 @@ Basic Commads
 
 /start :- check am Alive
 
-Bot Owner Only
-/broadcast Replay Any Message or Media
-/stats user Status
-/ban_user  click ban_user more info
-/unban_user click unban_user more info
-/banned_users Banned User Details
-/total How Many Files Added In Database
-/logger  Get Logs
-/delete Delete File From Database
+Admin only 
 
 """
 
